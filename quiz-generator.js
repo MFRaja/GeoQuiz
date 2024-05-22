dfcd2fef3021eed89bc34f5552eaee05a496ac19
FaiezRaja <f4iezraja@gmail.com>
2024-05-22 21:31:35 +0100

    removed inactive keys

--- quiz-generator.js
+++ quiz-generator.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const pos = require('pos')
 
 const QUIZ_LENGTH = 10
-const API_KEY = 'AIzaSyChzAGrXXV8gklFuucKcuT_dY0lOg5Fd84'
+const API_KEY = ''
 
 let allowedData = ["rating", "photos", "reviews", "reviews"]  //extra weighting for reviews
 const backupWords = ['Amazing!', 'nice', 'horrible,', 'staff', 'food', 'drink', 'Disappointing']  //in case we run out of random words
